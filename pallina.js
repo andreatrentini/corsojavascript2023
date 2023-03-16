3ab83620b84418953fc09b72b0a659016f2b4e42
andreatrentini <55395467+andreatrentini@users.noreply.github.com>
2023-03-16 10:13:56 +0000

    Esercizio finito con una pallina

--- pallina.js
+++ pallina.js
@@ -1,20 +1,43 @@
-var pallina = document.getElementById('pallina');
-var posizione = {
-    top: 0,
-    left: 0,
-    getTop()  {
-        return this.top + 'px';
+var pallinaHTML = document.getElementById('pallina');
+
+var pallina = {  
+    dimensione: {
+        width: 20,
+        height: 20
+    },  
+    posizione: {
+        top: 1,
+        left: 1,
+        getTop()  {
+            return this.top + 'px';
+        },
+        getLeft() {
+            return this.left + 'px'
+        }
     },
-    getLeft() {
-        return this.left + 'px'
+    spostamento: {
+        orizzontale: 1,
+        verticale: 1
+    },
+    immagine: pallinaHTML,
+
+    sposta(limiti) {
+        // controllo posizione
+        if (this.posizione.left <= 0 || this.posizione.left >= limiti.innerWidth - this.dimensione.width) {
+            this.spostamento.orizzontale = -1 * this.spostamento.orizzontale;
+        }
+
+        if (this.posizione.top <= 0 || this.posizione.top >= limiti.innerHeight - this.dimensione.height) {
+            this.spostamento.verticale *= -1;
+        }
+        this.posizione.left += this.spostamento.orizzontale;
+        this.posizione.top += this.spostamento.verticale;
+        
+        this.immagine.style.left = this.posizione.getLeft();
+        this.immagine.style.top = this.posizione.getTop();
     }
 }
 
 setInterval(() => {
-    console.log(posizione.getLeft());
-    // Codice da eseguire alla scadena di ogni intervallo di tempo
-    posizione.left++;
-    posizione.top++;
-    pallina.style.top = posizione.getTop();
-    pallina.style.left = posizione.getLeft();
+    pallina.sposta(window);
 }, 15);
