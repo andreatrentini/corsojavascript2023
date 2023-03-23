2353dd025976642b40a8e359dbbcecd95e4b9bbf
andreatrentini <55395467+andreatrentini@users.noreply.github.com>
2023-03-23 11:11:56 +0000

    Recuperato stash

--- test.js
+++ test.js
@@ -1,8 +1,6 @@
 // File aggiunto per testare e risolvere il problema git riscontrato
 var palline;
 
-function popola() {
-    for (let i = 0; i < 10; i++) {
-        palline.push(i);
-    }
+function aggiunta() {
+    return true;
 }
